83e9cf9b15b1d48f68372081c816c503e0c1235b
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-06-01 16:26:10 +0800

    Using Content Views

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Kelvin on 6/1/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
